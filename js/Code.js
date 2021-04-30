61292c678ca36ebc1fce8379286411c1face736c
dheerpadhiar <74356104+dheerpadhiar@users.noreply.github.com>
2021-04-30 18:09:16 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,28 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "DATABASE";
+const accessCode3 = "FUNCTION";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+    
+
+    fill("white")
+    textSize(15)
+    text("A T A D A S B A", 700,140)
+    fill("lightblue")
+    text("Hint: A place witch is like your brain !!", 700,160)
+
+
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 100,330)
+    fill("lightblue")
+    text("Hint: Performs a task 🧐🧐 !!", 100,350)
+
+
+}   
